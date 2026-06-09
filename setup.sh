9dbe536b6db99077212f44d8631585bc2e8125f6
mspirit1 <pabloloschi200@gmail.com>
2026-06-09 15:32:41 -0300

    setup script just in case

--- setup.sh
+++ setup.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+mkdir -p ~/.config/alacritty
+mkdir -p ~/.config/fastfetch
+mkdir -p ~/.config/sway
+mkdir -p ~/.config/fuzzel
+mkdir -p ~/.config/waybar
+
 cp alacritty.toml ~/.config/alacritty
 cp fastfetch_config.jsonc config.jsonc && mv config.jsonc ~/.config/fastfetch
 cp sway_config config && mv config ~/.config/sway
